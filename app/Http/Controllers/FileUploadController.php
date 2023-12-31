07fc546841800ae159c40fb09c39e643f86f06df
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2023-12-31 19:10:20 +0600

    pause code

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Google\Client as GoogleClient;
 
 class FileUploadController extends Controller
 {
@@ -11,9 +12,26 @@ class FileUploadController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $path = storage_path('app/public/google/client_secret_797725516027-ej9hkcm113k73rc2ktgbpcg4mtkhe2mo.apps.googleusercontent.com.json');
+        $credential = storage_path('app/public/google/client_secret_797725516027-ej9hkcm113k73rc2ktgbpcg4mtkhe2mo.apps.googleusercontent.com.json');
+        $client = new GoogleClient();
+        $client->setAuthConfig($credential); // Path to your downloaded credentials
+//        $client->setAccessType('offline');
+        $client->setAccessToken('1//04D1JEBOETZFeCgYIARAAGAQSNwF-L9IroT7Y2jQCSFzD-XGtCyjl0Z7Dygi_zvf-6sD-KdBOLW65N-ESkb5ktuDGwkLj8cU6i60'); // You'll need to obtain this token
 
-        dd($path);
+
+        // Create Google Drive service
+        $service = new \Google_Service_Drive($client);
+
+        $fileId = '1wY1CNTJPAuIpLBDHcwtmqWegMzxowJSEBxRvi-weopY';
+
+        // Download image
+        $response = $service->files->get($fileId, ['alt' => 'media']);
+
+        // Save the image locally
+        $imagePath = storage_path("app/public/images/{$fileId}.jpg");
+        file_put_contents($imagePath, $response->getBody()->getContents());
+
+        return $imagePath;
 
     }
 }
